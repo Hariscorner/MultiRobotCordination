035dde7502e7a06ac12e3914060bdc2b32b61ba7
harisCorner <harip.research@gmail.com>
2017-11-25 16:18:52 +0100

    Three blocks moving towards each other at manually given velocities

--- src/pubvel.cpp
+++ src/pubvel.cpp
@@ -1,34 +1,101 @@
 // This program publishes randomly−generated velocity messages for turtlesim
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
+//#include <geometry_msgs/Twist.h>
+#include <gazebo_msgs/ModelState.h>
 #include <stdlib.h> //For rand() and RAND_MAX
+#include "gazebo_msgs/GetModelState.h" 	//to use the getmodelstate service. alternatively, could have subscribed directly to /gazebo/model/states
 
 int main(int argc, char **argv){
-//Initialise the ROS system and become a node
-ros::init(argc, argv, "publish_velocity");
-ros::NodeHandle nh;
+	//Initialise the ROS system and become a node
+	ros::init(argc, argv, "publish_velocity");
+	ros::NodeHandle nh;
 
-//Create a publisher object
-ros::Publisher pub=nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
+	//Create a publisher object
+	//ros::Publisher pub=nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
+	ros::Publisher pub=nh.advertise<gazebo_msgs::ModelState>("gazebo/set_model_state", 1000);
+	gazebo_msgs::ModelState msg1;
+	gazebo_msgs::ModelState msg2;
+	gazebo_msgs::ModelState msg3;
+	msg1.model_name="box1";
+	msg2.model_name="box2";
+	msg3.model_name="box3";
+	
+	ros::ServiceClient gms_c = nh.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+    gazebo_msgs::GetModelState getmodelstate1;
+	gazebo_msgs::GetModelState getmodelstate2;
+	gazebo_msgs::GetModelState getmodelstate3;
+    getmodelstate1.request.model_name="box1";
+	getmodelstate2.request.model_name="box2";
+	getmodelstate3.request.model_name="box3";
+	
+	//seed the random number generator
+	srand(time(0));
 
-//seed the random number generator
-srand(time(0));
+	//Loop at 2Hz until the node is shutdown
+	ros::Rate rate(2);
+	while(ros::ok()){
+		//Create and fill in the message. The other four fields which are ignored by turtlesim, default ot 0
+		//geometry_msgs::Twist msg;
 
-//Loop at 2Hz until the node is shutdown
-ros::Rate rate(2);
-while(ros::ok()){
-	//Create and fill in the message. The other four fields which are ignored by turtlesim, default ot 0
-	geometry_msgs::Twist msg;
-	msg.linear.x=double(rand())/double(RAND_MAX);
-	msg.angular.z=double(rand())/double(RAND_MAX)-1;
-	
-	//Publish the messafe
-	pub.publish(msg);
-	
-	//send a message to rosout with the details
-	ROS_INFO_STREAM("sending_random_vel_command:"<<"_linear="<<msg.linear.x<<"_angular="<<msg.angular.z);
-	
-	//wait until it's time for another iteration
-	rate.sleep();
-	}
+		
+		gms_c.call(getmodelstate1);
+		gms_c.call(getmodelstate2);
+		gms_c.call(getmodelstate3);
+		
+		msg1.pose.position.x=getmodelstate1.response.pose.position.x;
+		msg1.pose.position.y=getmodelstate1.response.pose.position.y;
+		msg1.pose.position.z=getmodelstate1.response.pose.position.z;
+		msg1.pose.orientation.x=getmodelstate1.response.pose.orientation.x;
+		msg1.pose.orientation.y=getmodelstate1.response.pose.orientation.y;
+		msg1.pose.orientation.z=getmodelstate1.response.pose.orientation.z;
+		msg1.pose.orientation.w=getmodelstate1.response.pose.orientation.w;
+			
+		msg1.twist.linear.x=-3;
+		msg1.twist.linear.y=0;
+		msg1.twist.linear.z=0;
+		msg1.twist.angular.x=0;
+		msg1.twist.angular.y=0;
+		msg1.twist.angular.z=0;
+
+		msg2.pose.position.x=getmodelstate2.response.pose.position.x;
+		msg2.pose.position.y=getmodelstate2.response.pose.position.y;
+		msg2.pose.position.z=getmodelstate2.response.pose.position.z;
+		msg2.pose.orientation.x=getmodelstate2.response.pose.orientation.x;
+		msg2.pose.orientation.y=getmodelstate2.response.pose.orientation.y;
+		msg2.pose.orientation.z=getmodelstate2.response.pose.orientation.z;
+		msg2.pose.orientation.w=getmodelstate2.response.pose.orientation.w;
+			
+		msg2.twist.linear.x=1.5;
+		msg2.twist.linear.y=-1.5;
+		msg2.twist.linear.z=0;
+		msg2.twist.angular.x=0;
+		msg2.twist.angular.y=0;
+		msg2.twist.angular.z=0;
+		
+		
+		msg3.pose.position.x=getmodelstate3.response.pose.position.x;
+		msg3.pose.position.y=getmodelstate3.response.pose.position.y;
+		msg3.pose.position.z=getmodelstate3.response.pose.position.z;
+		msg3.pose.orientation.x=getmodelstate3.response.pose.orientation.x;
+		msg3.pose.orientation.y=getmodelstate3.response.pose.orientation.y;
+		msg3.pose.orientation.z=getmodelstate3.response.pose.orientation.z;
+		msg3.pose.orientation.w=getmodelstate3.response.pose.orientation.w;
+			
+		msg3.twist.linear.x=1.5;
+		msg3.twist.linear.y=1.5;
+		msg3.twist.linear.z=0;
+		msg3.twist.angular.x=0;
+		msg3.twist.angular.y=0;
+		msg3.twist.angular.z=0;
+		//Publish the messafe
+		pub.publish(msg1);
+		pub.publish(msg2);
+		pub.publish(msg3);
+
+		//send a message to rosout with the details
+		ROS_INFO_STREAM("sending_vel_command:"<<"_linear="<<msg1.twist.linear.x<<"_angular="<<msg1.twist.angular.z);
+
+		//wait until it's time for another iteration
+		rate.sleep();
+		}
 }
--- src/simple_world_plugin.cpp
+++ src/simple_world_plugin.cpp
@@ -1,30 +1,48 @@
-#include <gazebo/common/Plugin.hh>
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <stdio.h>
 #include <ros/ros.h>
 
-
-//commenting
 namespace gazebo
 {
-class WorldPluginTutorial : public WorldPlugin
-{
-public:
-  WorldPluginTutorial() : WorldPlugin()
+  class ModelPush : public ModelPlugin
   {
-  }
-
-  void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
-  {
-    // Make sure the ROS node for Gazebo has already been initialized                                                                                    
-    if (!ros::isInitialized())
+    public: void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
     {
-      ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
-        << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
-      return;
-    }
+      if (!ros::isInitialized())
+      {
+        ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
+          << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+        return;
+      }
 
     ROS_INFO("Hello World!");
-  }
+      
+      // Store the pointer to the model
+      this->model = _parent;
+      this->model->SetGravityMode(false);
+      // Listen to the update event. This event is broadcast every
+      // simulation iteration.
+      this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+          boost::bind(&ModelPush::OnUpdate, this, _1));
+    }
 
-};
-GZ_REGISTER_WORLD_PLUGIN(WorldPluginTutorial)
+    // Called by the world update start event
+    public: void OnUpdate(const common::UpdateInfo & /*_info*/)
+    {
+      // Apply a small linear velocity to the model.
+      //this->model->SetLinearVel(math::Vector3(.03, 0, 0));
+    }
+
+    // Pointer to the model
+    private: physics::ModelPtr model;
+
+    // Pointer to the update event connection
+    private: event::ConnectionPtr updateConnection;
+  };
+
+  // Register this plugin with the simulator
+  GZ_REGISTER_MODEL_PLUGIN(ModelPush)
 }
