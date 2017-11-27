d8df37c897f869b7b9bd690cc0b0c536081241e5
harisCorner <harip.research@gmail.com>
2017-11-27 13:45:12 +0100

    getCurrState () added and tested succesfully

--- src/pubvel.cpp
+++ src/pubvel.cpp
@@ -9,27 +9,25 @@
 #define NROBOT 3
 
 double constrainAngle(double x);
+void getCurrState(gazebo_msgs::GetModelState getmodelstate[], int m, gazebo_msgs::ModelState state[]);
 
 int main(int argc, char **argv){
 	//Initialise the ROS system and become a node
 	ros::init(argc, argv, "publish_velocity");
 	ros::NodeHandle nh;
 	
-	tf::Pose pose;
-	double yaw_angle;
-	
 	//Create a publisher object
 	//ros::Publisher pub=nh.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
 	ros::Publisher pub=nh.advertise<gazebo_msgs::ModelState>("gazebo/set_model_state", 1000);
-	gazebo_msgs::ModelState msg[NROBOT];
+	gazebo_msgs::ModelState state[NROBOT];
 	
 	ros::ServiceClient gms_c = nh.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
 	gazebo_msgs::GetModelState getmodelstate[NROBOT];
 
 	
-	msg[0].model_name="myrobot1";
-	msg[1].model_name="myrobot2";
-	msg[2].model_name="myrobot3";
+	state[0].model_name="myrobot1";
+	state[1].model_name="myrobot2";
+	state[2].model_name="myrobot3";
 	
     getmodelstate[0].request.model_name="myrobot1";
 	getmodelstate[1].request.model_name="myrobot2";
@@ -47,28 +45,8 @@ int main(int argc, char **argv){
 
 		for (int m=0; m<NROBOT; ++m) {
 			gms_c.call(getmodelstate[m]);
-
-			msg[m].pose.position.x=getmodelstate[m].response.pose.position.x;
-			msg[m].pose.position.y=getmodelstate[m].response.pose.position.y;
-			msg[m].pose.position.z=getmodelstate[m].response.pose.position.z;
-			msg[m].pose.orientation.x=getmodelstate[m].response.pose.orientation.x;
-			msg[m].pose.orientation.y=getmodelstate[m].response.pose.orientation.y;
-			msg[m].pose.orientation.z=getmodelstate[m].response.pose.orientation.z;
-			msg[m].pose.orientation.w=getmodelstate[m].response.pose.orientation.w;
-		
-  			tf::poseMsgToTF(getmodelstate[m].response.pose, pose);
-  			yaw_angle = tf::getYaw(pose.getRotation());
-			yaw_angle = constrainAngle(yaw_angle + M_PI);
-			//ROS_INFO_STREAM("yaw (" <<m << ") : " <<yaw_angle);
-			
-			msg[m].twist.linear.x=3*cos(yaw_angle);
-			msg[m].twist.linear.y=3*sin(yaw_angle);
-			msg[m].twist.linear.z=0;
-			msg[m].twist.angular.x=0;
-			msg[m].twist.angular.y=0;
-			msg[m].twist.angular.z=0;
-			pub.publish(msg[m]);
-			ROS_INFO_STREAM("sending_vel_command:"<<"_linear="<<msg[m].twist.linear.x<<"_angular="<<msg[m].twist.linear.y);
+			getCurrState(getmodelstate, m, state);
+			pub.publish(state[m]);
 		}
 
 		//wait until it's time for another iteration
@@ -81,4 +59,33 @@ double constrainAngle(double x){		//Normalize to [-180,180)
     if (x < 0)
         x += 2*M_PI;
     return x - M_PI;
+}
+
+void getCurrState(gazebo_msgs::GetModelState getmodelstate[], int m, gazebo_msgs::ModelState state[]) {
+
+	tf::Pose pose;
+	double yaw_angle;
+	
+	state[m].pose.position.x=getmodelstate[m].response.pose.position.x;
+	state[m].pose.position.y=getmodelstate[m].response.pose.position.y;
+	state[m].pose.position.z=getmodelstate[m].response.pose.position.z;
+	state[m].pose.orientation.x=getmodelstate[m].response.pose.orientation.x;
+	state[m].pose.orientation.y=getmodelstate[m].response.pose.orientation.y;
+	state[m].pose.orientation.z=getmodelstate[m].response.pose.orientation.z;
+	state[m].pose.orientation.w=getmodelstate[m].response.pose.orientation.w;
+
+	tf::poseMsgToTF(getmodelstate[m].response.pose, pose);
+	yaw_angle = tf::getYaw(pose.getRotation());
+	yaw_angle = constrainAngle(yaw_angle + M_PI);
+	//ROS_INFO_STREAM("yaw (" <<m << ") : " <<yaw_angle);
+
+	state[m].twist.linear.x=3*cos(yaw_angle);
+	state[m].twist.linear.y=3*sin(yaw_angle);
+	state[m].twist.linear.z=0;
+	state[m].twist.angular.x=0;
+	state[m].twist.angular.y=0;
+	state[m].twist.angular.z=0;
+
+	ROS_INFO_STREAM("sending_vel_command:"<<"_linear="<<state[m].twist.linear.x<<"_angular="<<state[m].twist.linear.y);
+	return;
 }
